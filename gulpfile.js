5e89ad3f1e91443639ad70feb0b402e1e8804cc1
betodasilva <jrs_robert@hotmail.com>
2017-09-30 22:59:47 -0300

    Add initial files, w/ gulp (minify, uglify, watcher)

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass');
+
+
+// uglify JavaScripts
+gulp.task('uglify', function(){
+    gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('minify', function(){
+    gulp.src('css/sass/main.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest('build/css'));
+});
+
+//watch js files
+gulp.task('watch', function(){
+    gulp.watch('js/*.js', ['uglify']);
+    gulp.watch('css/sass/*.scss', ['minify']);
+});
+
+gulp.task('default', ['uglify', 'minify', 'watch']);
---
+++ js/main.js
@@ -0,0 +1,6 @@
+function sayHello(){
+    console.log('hello world');
+    console.log('hello again');
+};
+
+//
